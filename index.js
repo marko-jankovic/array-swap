c95d67b381cd3d42ac3aa3687627d32c2c35eaa4
Marko Jankovic <markoj@vast.com>
2014-04-23 21:52:10 +0200

    Extending Array.prototype

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 if (!Array.prototype.swap) {
 
-	module.exports = Array.prototype.swap = function(x, y) {
+	var swap = function(x, y) {
 
 		this[x] = this.splice(y, 1, this[x])[0];
-    	return this;
-
-	}(Array.prototype.swap);
-}	
+		return this;
+	};
+	
+	if (Object.defineProperty) {
+		try {
+			Object.defineProperty(Array.prototype, 'swap', {
+				value: swap,
+				configurable: true, 
+				enumerable: false,
+				writable: true
+			});
+	    } catch(e) {}
+	}
+	
+	Array.prototype.swap = swap;
+	
+	module.exports =  swap;
+}
