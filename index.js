7d365544ff6ab034a9b3ade72190efdee46d1b1a
Marko Jankovic <markoj@vast.com>
2014-04-23 20:16:13 +0200

    added Array.prototype.swap

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = Array.prototype.swap = function(x, y) {
+if (!Array.prototype.swap) {
 
-}(Array.prototype.swap);
+	module.exports = Array.prototype.swap = function(x, y) {
+
+		this[x] = this.splice(y, 1, this[x])[0];
+    	return this;
+
+	}(Array.prototype.swap);
+}	
